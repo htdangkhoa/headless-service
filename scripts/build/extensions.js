6137a558c7939f7af0d5c6a30a79c9d813d34a1f
KhoaHTD <khoa.htd@amanotes.com>
2025-10-04 14:11:51 +0700

    refactor: clean up extension build scripts
    - Removed deprecated recorder.js file and its associated build logic to streamline the extension build process.
    - Commented out unused code in extensions.js to improve readability and maintainability.

--- scripts/build/extensions.js
+++ scripts/build/extensions.js
@@ -6,42 +6,6 @@ import * as glob from 'glob';
 
 const cwd = process.cwd();
 
-// const extensionsDir = join(cwd, 'src', 'shared', 'extensions');
-
-// const entryDir = join(cwd, 'src', 'shared', 'extensions');
-
-// const entryPoints = fs
-//   .readdirSync(entryDir)
-//   .filter((file) => file.endsWith('.ts'))
-//   .map((file) => join(cwd, 'src', 'shared', 'recorder', file));
-
-// const outdir = join(cwd, 'extensions', 'recorder');
-
-// const manifestContent = fs.readFileSync(join(entryDir, 'manifest.json'), 'utf-8');
-
-// async function main() {
-//   await build({
-//     bundle: true,
-//     entryPoints,
-//     metafile: true,
-//     outdir,
-//     plugins: [
-//       polyfillNode({
-//         globals: {
-//           process: true,
-//         },
-//       }),
-//     ],
-//   });
-
-//   fs.writeFileSync(join(outdir, 'manifest.json'), manifestContent);
-// }
-
-// main().catch((e) => {
-//   console.error(e);
-//   process.exit(1);
-// });
-
 async function buildExtension(extensionDir) {
   const dirName = extensionDir.split('/').pop();
 
